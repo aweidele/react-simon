2e5dfc8070b561d82c33b5287d332ee81e52f1ce
Aaron Weidele <>
2024-06-24 15:46:59 -0400

    create-react

---
+++ src/App.js
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Start of the Simon game</h1>
+      </header>
+    </div>
+  );
+}
