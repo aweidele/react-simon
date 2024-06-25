c7fd84486a8acee45a292cc1593cfbc702438f43
Aaron Weidele <>
2024-06-25 06:17:53 -0400

    starting layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import ButtonSvg from "./components/ButtonSvg";
+
 export default function App() {
   return (
-    <div className="App">
+    <div className="max-w-4xl mx-auto">
       <header className="App-header">
         <h1>Start of the Simon game</h1>
+        <ButtonSvg />
       </header>
     </div>
   );
