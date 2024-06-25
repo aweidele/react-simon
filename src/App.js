ac22b4222c008767ca36788d0a9f9f3ec8647b0a
Aaron Weidele <>
2024-06-25 14:52:57 -0400

    stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import GameBoard from "./components/GameBoard";
+
 export default function App() {
   return (
     <div className="max-w-2xl mx-auto">
       <header className="App-header">
         <h1 className="text-center text-2xl">Start of the Simon game</h1>
-        <GameBoard />
       </header>
+      <GameBoard />
     </div>
   );
 }
