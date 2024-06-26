c8d33b0657a14b52a0fea8bfbad36316449cc40d
Aaron Weidele <>
2024-06-26 12:27:35 -0400

    Sequence state

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -5,6 +5,7 @@ import dummySequence from "../data/dummy-sequence";
 const clr = ["green", "red", "blue", "yellow"];
 
 export default function GameBoard() {
+  const [sequence, setSeqence] = useState(dummySequence);
   const [sequenceTurn, setSequenceTurn] = useState(0);
   const [gameState, setGameState] = useState("playback-on");
 
@@ -16,7 +17,7 @@ export default function GameBoard() {
     if (gameState === "playback-on") {
       setTimeout(() => {
         const nextTurn = sequenceTurn + 1;
-        if (nextTurn < dummySequence.length) {
+        if (nextTurn < sequence.length) {
           setSequenceTurn((prevSequence) => nextTurn);
           setGameState("playback-pause");
         } else {
@@ -41,20 +42,20 @@ export default function GameBoard() {
   return (
     <>
       <div className="grid grid-cols-2 grid-rows-2 my-8 p-4 rounded-full bg-slate-700">
-        <GameButton key="lime" color="lime" extraClasses="order-1" playbackRate={0.8} disabled={!isPlayerTurn} active={isPlaybackOn && dummySequence[sequenceTurn] === 0} />
-        <GameButton key="red" color="red" extraClasses=" order-2 rotate-90" playbackRate={1} disabled={!isPlayerTurn} active={isPlaybackOn && dummySequence[sequenceTurn] === 1} />
-        <GameButton key="blue" color="blue" extraClasses="order-4 rotate-180" playbackRate={1.2} disabled={!isPlayerTurn} active={isPlaybackOn && dummySequence[sequenceTurn] === 2} />
-        <GameButton key="yellow" color="yellow" extraClasses="order-3 -rotate-90" playbackRate={1.4} disabled={!isPlayerTurn} active={isPlaybackOn && dummySequence[sequenceTurn] === 3} />
+        <GameButton key="lime" color="lime" extraClasses="order-1" playbackRate={0.8} disabled={!isPlayerTurn} active={isPlaybackOn && sequence[sequenceTurn] === 0} />
+        <GameButton key="red" color="red" extraClasses=" order-2 rotate-90" playbackRate={1} disabled={!isPlayerTurn} active={isPlaybackOn && sequence[sequenceTurn] === 1} />
+        <GameButton key="blue" color="blue" extraClasses="order-4 rotate-180" playbackRate={1.2} disabled={!isPlayerTurn} active={isPlaybackOn && sequence[sequenceTurn] === 2} />
+        <GameButton key="yellow" color="yellow" extraClasses="order-3 -rotate-90" playbackRate={1.4} disabled={!isPlayerTurn} active={isPlaybackOn && sequence[sequenceTurn] === 3} />
       </div>
       <div className="absolute top-2 left-2">
         <ol>
-          {dummySequence.map((turn, index) => (
+          {sequence.map((turn, index) => (
             <li className={index === sequenceTurn && "font-bold"}>
               {turn} - {clr[turn]}
             </li>
           ))}
         </ol>
-        <p>{dummySequence[sequenceTurn]}</p>
+        <p>{sequence[sequenceTurn]}</p>
         <p>{gameState}</p>
         <p>Is playing? {isPlayback ? "Yes" : "No"}</p>
         <button onClick={handleRestart} className="border p-2 my-8 hover:bg-slate-200">
